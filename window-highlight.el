2da1f3b603efd9e7404ce6d04d025ef4fa13c569
Daniel Colascione <dancol@dancol.org>
2025-01-02 13:27:15 -0500

    seven years of updates

--- window-highlight.el
+++ window-highlight.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2018 Daniel Colascione
 
 ;; Author: Daniel Colascione <dancol@dancol.org>
-;; Version: 1.0
+;; Version: 2.0
 ;; Keywords: faces
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -55,110 +55,89 @@
   "Face used to highlight the focused window"
   :group 'window-highlight)
 
-(defvar window-highlight--selected-window nil
-  "Cache of last window we knew to be selected")
-
-(defvar window-highlight--need-rescan nil
-  "True when we need post-command-hook to refresh internal data")
-
 (defvar-local window-highlight--face-remap-cookies nil
   "Face-remapping cookies applied to current buffer")
 
 (defvar window-highlight-mode)
+(defvar window-highlight--current-buffer)
+(defvar window-highlight--selected-window)
+
+(defconst window-highlight--has-focus-property
+  'window-highlight-has-focus)
 
 (defun window-highlight--has-keyboard-focus-p (window)
-  (and (frame-focus-state (window-frame window))
-       (eq window (frame-selected-window window))))
+  ;; The AND below forces the value to be EQ t if it's non-nil, which
+  ;; makes the tty case (for which we report unknown focus)
+  ;; work correctly.
+  (and (frame-focus-state (window-frame window)) t))
 
-(defun window-highlight--rescan-windows ()
-  (setf window-highlight--need-rescan nil)
+(defun window-highlight--scan-for-focus ()
   (let ((focused-buffer nil))
     (walk-windows
      (lambda (window)
        (let ((has-keyboard-focus
               (and window-highlight-mode
+                   (eq (selected-window) window)
                    (window-highlight--has-keyboard-focus-p window))))
-         (set-window-parameter window 'has-keyboard-focus has-keyboard-focus)
+         (set-window-parameter
+          window window-highlight--has-focus-property has-keyboard-focus)
          (when has-keyboard-focus
            (setf focused-buffer (window-buffer window)))))
      t t)
-    (dolist (buffer (buffer-list))
-      (with-current-buffer buffer
-        (cond ((eq buffer focused-buffer)
-               (unless window-highlight--face-remap-cookies
-                 (setf window-highlight--face-remap-cookies
-                       (mapcar (lambda (face)
-                                 (face-remap-add-relative
-                                  face '(:filtered (:window has-keyboard-focus t)
-                                         window-highlight-focused-window)))
-                               '(default fringe)))))
-              (t
-               (when window-highlight--face-remap-cookies
-                 (mapc #'face-remap-remove-relative
-                       window-highlight--face-remap-cookies)
-                 (setf window-highlight--face-remap-cookies nil))))))
-    (redraw-display)))
+    focused-buffer))
+
+(defun window-highlight--update-remappings (focused-buffer)
+  (dolist (buffer (buffer-list))
+    (with-current-buffer buffer
+      (cond ((eq buffer focused-buffer)
+             (unless window-highlight--face-remap-cookies
+               (setf window-highlight--face-remap-cookies
+                     (mapcar
+                      (lambda (face)
+                        (face-remap-add-relative
+                         face `(:filtered
+                                (:window
+                                 ,window-highlight--has-focus-property
+                                 t)
+                                window-highlight-focused-window)))
+                      '(default fringe)))))
+            (t
+             (when window-highlight--face-remap-cookies
+               (mapc #'face-remap-remove-relative
+                     window-highlight--face-remap-cookies)
+               (setf window-highlight--face-remap-cookies nil)))))))
+
+(defun window-highlight--rescan-windows ()
+  (let ((focused-buffer (window-highlight--scan-for-focus)))
+    (window-highlight--update-remappings focused-buffer)
+    (let ((sb (current-buffer)) (sw (selected-window)))
+      (unless (and (eq window-highlight--selected-window sw)
+                   (eq window-highlight--current-buffer sb))
+        ;; Work around problem with fringe not updating.
+        ;; Somehow, forcing this redraw fixes the problem.
+        (when nil (redraw-display))
+        (setf window-highlight--selected-window sw)
+        (setq window-highlight--current-buffer sb)))))
 
 (defun window-highlight--pre-redisplay (_window)
-  (when window-highlight--need-rescan
-    (window-highlight--rescan-windows)))
-
-(defun window-highlight--window-configuration-change-hook ()
-  (setf window-highlight--need-rescan t))
-
-(defun window-highlight--buffer-list-update-hook ()
-  (let ((old-window window-highlight--selected-window))
-    (unless (eq old-window (selected-window))
-      (setf window-highlight--selected-window (selected-window))
-      (setf window-highlight--need-rescan t))))
-
-(defun window-highlight--make-frame-function (frame)
-  (unless (display-graphic-p frame)
-    (setf window-highlight--need-rescan t)))
-
-(defun window-highlight--after-focus-change-function ()
-  (setf window-highlight--need-rescan t))
-
-(defun window-highlight--change-major-mode ()
-  (setf window-highlight--need-rescan t))
+  (window-highlight--rescan-windows))
 
 ;;;###autoload
 (define-minor-mode window-highlight-mode
-  "Apply a face to the window with keyboard focus" nil nil nil
+  "Apply a face to the window with keyboard focus"
+  :lighter nil
   :group 'window-highlight
   :global t
-  (cond (window-highlight-mode
-         (set-frame-parameter nil 'wh--have-input-focus t)
-         (add-hook 'window-configuration-change-hook
-           #'window-highlight--window-configuration-change-hook)
-         (add-hook 'buffer-list-update-hook
-           #'window-highlight--buffer-list-update-hook)
-         (add-hook 'after-make-frame-functions
-           #'window-highlight--make-frame-function)
-         (add-hook 'change-major-mode-hook
-           #'window-highlight--change-major-mode)
-         (add-function :after after-focus-change-function
-                       #'window-highlight--after-focus-change-function)
-         (add-function :after pre-redisplay-function
-                       #'window-highlight--pre-redisplay)
-         )
-        (t
-         (mapc (lambda (frame)
-                 (set-frame-parameter frame 'wh--have-input-focus nil))
-               (frame-list))
-         (remove-hook 'window-configuration-change-hook
-                      #'window-highlight--window-configuration-change-hook)
-         (remove-hook 'buffer-list-update-hook
-                      #'window-highlight--buffer-list-update-hook)
-         (remove-hook 'after-make-frame-functions
-                      #'window-highlight--make-frame-function)
-         (remove-hook 'change-major-mode-hook
-                      #'window-highlight--change-major-mode)
-         (remove-function after-focus-change-function
-                          #'window-highlight--after-focus-change-function)
-         (remove-function pre-redisplay-function
-                          #'window-highlight--pre-redisplay)))
-  (window-highlight--rescan-windows))
+  (progn
+    (cond (window-highlight-mode
+           (setf window-highlight--current-buffer nil)
+           (setf window-highlight--selected-window nil)
+           (add-function :after pre-redisplay-function
+                         #'window-highlight--pre-redisplay))
+          (t
+           (remove-function pre-redisplay-function
+                            #'window-highlight--pre-redisplay)))
+    (window-highlight--rescan-windows)))
 
 (provide 'window-highlight)
 
